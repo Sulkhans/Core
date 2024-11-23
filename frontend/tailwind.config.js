83ee9ab9567ec63271ef440aecabbbfc676d1688
Sulkhan <suxozoidze2004@gmail.com>
2024-11-23 23:05:19 +0400

    ~

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        core: {
+          main: "#0068b5",
+          light: "#0085e8",
+          dark: "#004b82",
+          white: "#f8fafc",
+        },
+      },
+    },
   },
   plugins: [],
 };
