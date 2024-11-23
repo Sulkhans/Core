a8ac69acc2cbcdf19263f6cd82866ce3d91a1e89
Sulkhan <suxozoidze2004@gmail.com>
2024-11-23 23:05:07 +0400

    Routes setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Layout from "./components/Layout.tsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
