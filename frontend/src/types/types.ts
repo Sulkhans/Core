0aa2fe7fb6c61d18b1cae500ef5b9441b43c22d2
Sulkhan <suxozoidze2004@gmail.com>
2024-12-09 20:29:49 +0400

    Product types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -4,3 +4,53 @@ export type ErrorType = {
     message: string;
   };
 };
+
+export type ProductBaseType = {
+  name: string;
+  image: string;
+  brand: string;
+  price: number;
+  category: string;
+  inStock: number;
+};
+
+export type PhoneType = {
+  color: string;
+  screenSize: number;
+  screenType: string;
+  screenBrightness: number;
+  refreshRate: number;
+  operatingSystem: string;
+  chipset: string;
+  ram: number;
+  storageCapacity: number;
+  sdSlot: boolean;
+  mainCamera: number;
+  frontCamera: number;
+  batteryCapacity: number;
+  wirelessCharging: boolean;
+  weight: number;
+};
+
+export type LaptopType = {
+  color: string;
+  model: string;
+  screenSize: number;
+  screenType: string;
+  screenBrightness: number;
+  refreshRate: number;
+  resolution: string;
+  touchScreen: boolean;
+  cpu: string;
+  cores: number;
+  threads: number;
+  ram: number;
+  ramType: string;
+  storageCapacity: number;
+  gpuType: string;
+  gpu: string;
+  vram?: number;
+  operatingSystem: string;
+  keyboardLighting: boolean;
+  weight: number;
+};
