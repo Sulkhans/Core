72f9b41ed14c7ee3daca3fd4233c06815659cdd4
Sulkhan <suxozoidze2004@gmail.com>
2024-12-19 00:53:00 +0400

    css

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useRef } from "react";
-import { useGetProductByIdQuery } from "../redux/api/productApiSlice";
+import {
+  useGetProductByIdQuery,
+  useGetRandomProductsQuery,
+} from "../redux/api/productApiSlice";
 import { BASE_URL } from "../redux/constants";
 import Loader from "../components/Loader";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,6 +16,7 @@ import { addToCart } from "../redux/slices/cartSlice";
 import { toast } from "react-toastify";
 import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import ProductSection from "../components/ProductSection";
 //@ts-ignore
 import "swiper/css";
 //@ts-ignore
@@ -25,6 +29,7 @@ const Product = () => {
   const { data: product, isLoading } = useGetProductByIdQuery(id);
   const { favorites } = useSelector((state: RootState) => state.favorites);
   const { cartItems } = useSelector((state: RootState) => state.cart);
+  const { data: randomProducts } = useGetRandomProductsQuery({});
   const dispatch = useDispatch<AppDispatch>();
 
   const isFavorite = favorites.some((item) => item._id === id);
@@ -62,10 +67,10 @@ const Product = () => {
   return isLoading ? (
     <Loader />
   ) : (
-    <main className="grid lg:grid-cols-2 lg:gap-4 xl:gap-8">
+    <main className="grid items-center lg:grid-cols-2 lg:gap-4 xl:gap-8">
       <section>
-        <div className="flex flex-col-reverse gap-8 lg:flex-row lg:gap-0">
-          <div className="flex justify-center gap-4 lg:flex-col">
+        <div className="flex items-center justify-center gap-2 flex-col-reverse sm:flex-row sm:gap-12 lg:gap-6 xl:gap-8">
+          <div className="flex justify-center gap-4 sm:flex-col">
             {product.images.map((img: string, i: number) => (
               <img
                 key={i}
@@ -80,7 +85,7 @@ const Product = () => {
             ref={swiperRef}
             navigation={true}
             modules={[Navigation]}
-            className="w-full max-w-xs sm:max-w-sm xl:max-w-md relative"
+            className="w-full max-w-xs sm:max-w-sm xl:max-w-md !mx-0 relative"
           >
             {product.images.map((img: string) => (
               <SwiperSlide key={img}>
@@ -91,46 +96,45 @@ const Product = () => {
                 />
               </SwiperSlide>
             ))}
-            <button
-              onClick={handleFavorite}
-              className="absolute right-0 bottom-0 lg:top-4 lg:bottom-auto lg:right-4 z-10"
-            >
-              <Heart
-                className={`${isFavorite && "fill-core-main"} stroke-core-main`}
-              />
-            </button>
           </Swiper>
         </div>
-        <div className="flex justify-center my-6 lg:my-5 lg:ml-20">
-          <button
-            onClick={handleCart}
-            className="w-full lg:max-w-sm xl:max-w-md text-nowrap py-2 px-6 rounded-md text-white bg-core-main hover:bg-core-dark active:bg-core-dark duration-300 transition-colors"
-          >
-            <span className="inline-block">Add to cart</span>
-            <Cart className="inline ml-2 mb-0.5" />
-          </button>
-        </div>
       </section>
-      <section>
+      <section className="mt-6 lg:mt-0">
         <h1 className="font-medium text-balance text-3xl">
           {product.brand !== product.name.split(" ")[0] && product.brand}{" "}
           {product.name}
         </h1>
-        <p className="font-semibold font-sintony text-2xl mt-2">
-          <span className="mr-0.5">$</span>
-          {product.price}
-        </p>
-        <h2 className="mt-4 mb-2 text-lg font-semibold font-sintony text-core-main">
+        <div className="mt-2 mb-6 sm:mb-2 sm:flex justify-between items-center">
+          <p className="font-semibold font-sintony text-2xl mb-4 sm:mb-0">
+            <span className="mr-0.5">$</span>
+            {product.price}
+          </p>
+          <div className="flex gap-4">
+            <button onClick={handleFavorite}>
+              <Heart
+                className={`${isFavorite && "fill-core-main"} stroke-core-main`}
+              />
+            </button>
+            <button
+              onClick={handleCart}
+              className="w-full sm:w-fit text-nowrap py-2 px-6 rounded-full text-white bg-core-main hover:bg-core-dark active:bg-core-dark duration-300 transition-colors"
+            >
+              <span className="inline-block">Add to cart</span>
+              <Cart className="inline ml-2 mb-0.5" />
+            </button>
+          </div>
+        </div>
+        <h2 className="mb-2 text-lg font-semibold font-sintony text-core-main">
           Specifications
         </h2>
-        <div className="max-h-[24rem] overflow-y-auto scroll pr-2">
+        <div className="max-h-[18rem] overflow-y-auto">
           <table className="w-full text-sm">
             <tbody>
               {Object.entries(product.details).map(([key, value]) =>
                 value ? (
                   <tr key={key} className="*:px-4 *:py-3.5 even:bg-core-white">
-                    <td className="rounded-l-md">{format(key)}</td>
-                    <td className="rounded-r-md">
+                    <td className="rounded-l-full">{format(key)}</td>
+                    <td className="rounded-r-full">
                       {typeof value === "boolean"
                         ? value
                           ? "Yes"
@@ -144,6 +148,9 @@ const Product = () => {
           </table>
         </div>
       </section>
+      <div className="overflow-hidden lg:col-span-2 my-6 lg:mt-2">
+        <ProductSection heading="Discover" products={randomProducts} />
+      </div>
     </main>
   );
 };
