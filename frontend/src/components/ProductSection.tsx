92160e9bef759f578b2f690201d3840717bfa2d7
Sulkhan <suxozoidze2004@gmail.com>
2024-12-26 00:28:42 +0400

    ~

--- frontend/src/components/ProductSection.tsx
+++ frontend/src/components/ProductSection.tsx
@@ -12,7 +12,7 @@ const ProductSection = ({ link, heading, products }: Props) => {
   return (
     <section>
       <h2 className="mb-4 text-xl font-sintony font-semibold text-core-main">
-        {link ? <Link to={`/${link}`}>{heading}</Link> : heading}
+        {link ? <Link to={`/${link}?page=1`}>{heading}</Link> : heading}
       </h2>
       <div className="flex justify-between gap-8 overflow-x-auto">
         {products &&
